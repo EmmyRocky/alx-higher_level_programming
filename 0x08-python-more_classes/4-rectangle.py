e5066f464b966127b736fc116cd40346944c8191
EmmyRocky <emmyrockyy@gmail.com>
2022-08-05 03:22:05 -0700

    4-rectangle.py

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+'''Learning more on Classes and Objects'''
+
+class Rectangle:
+    '''Class Rectangle'''
+    def __init__(self, width=0, height=0):
+        '''Configuring attributes'''
+        self.height = height
+        self.width = width
+
+        @property
+        def width(self):
+            '''Retrieving the value of width'''
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            '''Putting in Place the value of width'''
+            if not type(value) is int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+
+        @property
+        def height(self):
+            '''Retrieving the value of height'''
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            '''Setting the value of height'''
+            if not type(value) is int:
+                reaise TypeError("height must be set as an integer")
+                if value < 0:
+                    raise ValueError("height has to be >= 0")
+                self.__height = value
+
+        def area(self):
+            '''Defining a method to retrieve the area of the Rectangle'''
+            return (self.height * self.width)
+
+        def perimeter(self):
+            '''Defining a method to retrieve the perimeter of the Rectangle'''
+            if self.height == 0 or self.width == 0:
+                return 0
+            return ((self.height + self.width) * 2)
+
+        def __str__(self):
+            '''Retrieving __str__ method'''
+            if self.height == 0 or self.width ==0:
+                return ""
+            else:
+                printable = ((("#" * self.width) + "\n") * self.height)
+                return (printable[:-1])
+            # return(((("#" * self.width) + "\n") * self.height)[:-1])
+
+        def __repr__(self):
+            '''Defining __repr__ method'''
+            return "Rectangle({}, {})".format(self.width, self.height)
+            # return 'Rectangle'(' + str(self.width) + ', ' + str(self.height) + ')'
