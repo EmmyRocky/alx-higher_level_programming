dc470880de84b62962838d67e71d1c3186ad64b1
EmmyRocky <emmyrockyy@gmail.com>
2022-08-20 03:59:45 -0700

    100-calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 
 
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+    from calculator_1.py import add, sub, mul, div
     import sys
 
     args_sum = len(sys.argv) -1
