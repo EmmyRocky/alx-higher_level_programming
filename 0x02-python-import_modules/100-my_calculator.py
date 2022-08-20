73dd6e961db1c3ea5119281283020db404d157c5
EmmyRocky <emmyrockyy@gmail.com>
2022-08-20 04:41:03 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,13 +11,13 @@ if __name__ == "__main__" :
     if args_sum != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if sys.argv[2] not in operators:
+    if sys.argv [2] not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
-    a = sys.argv[1]
-    b = sys.argv[3]
-    operator = sys.argv[2]
+    a = sys.argv [1]
+    b = sys.argv [3]
+    operator = sys.argv [2]
     if operator == operators [0]:
         print("{} {} {} = {}".format(a, operator, b, add(int(a), int(b))))
     elif operator == operators [1]:
