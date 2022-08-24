fb08666f95441e458cb67b0cdb037982ec71bc42
EmmyRocky <emmyrockyy@gmail.com>
2022-08-24 02:26:14 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     """ Excluding all C's and c's and returning a new string """
-    new_string = my_string.translate({ord(i): None for i in 'cC})
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            new_string += my_string[i]
     return (new_string)
