fa23354e183a26fb9503ea641e663e5cdc9dfa75
EmmyRocky <emmyrockyy@gmail.com>
2022-08-30 09:34:07 -0700

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def divivsible_by_2(my_list=[]):
-    realbranded_list = list(my_list)
+    new_list = list(my_list)
 
     for r in range(len(my_list)):
         if my_list[r] % 2 == 0:
-            realbranded_list[r] = True
+            new_list[r] = True
         else:
-            realbranded_list[r] = False
-    return realbranded_list[r]
+            new_list[r] = False
+    return new_list[r]
