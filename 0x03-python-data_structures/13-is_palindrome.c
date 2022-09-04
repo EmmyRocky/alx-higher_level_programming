2d65d101d532e3a641d0ec354bdaa3e142064841
EmmyRocky <emmyrockyy@gmail.com>
2022-09-03 21:18:11 -0700

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,11 +1,17 @@
 #include "lists.h"
-#include <stdio.h>
 
-int comparato(listin_t **head, listint_t *last)
+/**
+ * comparator - compare the beginning and end positions
+ * @head: linked list
+ * @last: end of list
+ * Return:1 true, 0 false
+ */
+
+int comparator(listint_t **head, listint_t *last)
 {
 	if (last == NULL)
 		return (1);
-	if (comparator(head, last->next) && (*head)->n == last->n)
+	if (comparator(head, last->next) && (*head)-> == last->n)
 	{
 		*head = (*head)->next;
 		return (1);
@@ -13,10 +19,15 @@ int comparato(listin_t **head, listint_t *last)
 	return (0);
 }
 
+/**
+ * the function is_palindrome - confirms if a singly linked list is a palindrome
+ * @head: linked list itself
+ * Return: 1 true, 0 false
+ */
+
 int is_palindrome(listint_t **head)
 {
-	if (!head || !(**head))
+	if (!head || !(*head))
 		return (1);
 	return (comparator(head, *head));
 }
-
