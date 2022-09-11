77966190e409eb765c051ef598e6057d8ed721ea
EmmyRocky <emmyrockyy@gmail.com>
2022-09-11 14:45:00 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     dup_matrix = matrix.copy()
     for line in range(len(matrix)):
-        dup_matrix[line] = list(mat((lambda x: x**2), matrix[line]))
-        return(dup_matrix)
+        dup_matrix[line] = list(map((lambda x: x**2), matrix[line]))
+    return(dup_matrix)
