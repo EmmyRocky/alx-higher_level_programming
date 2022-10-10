067ae4396e9cfb3e1a2da2f57b65dfd84ea98007
EmmyRocky <emmyrockyy@gmail.com>
2022-10-10 08:04:40 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const message = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let y = 0; y < message.length; y++) {
-	console.log(message[y];
+for (let i = 0; i < message.length; i++) {
+	console.log(message[i];
 }
