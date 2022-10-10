7b8a26b0f23abf0cee5f43f355189fe9967e31c8
EmmyRocky <emmyrockyy@gmail.com>
2022-10-10 04:55:40 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,5 +3,5 @@ const len = process.argv.length - 2;
 const print = console.log;
 
 if (!len) print('No argument');
-if (len > 1) print('Arguments found');
 if (len === 1) print('Argument found');
+if (len > 1) print('Arguments found');
