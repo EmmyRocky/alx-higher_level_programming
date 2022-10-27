d853c346f337f5859943ba2de98f048ba4608037
EmmyRocky <emmyrockyy@gmail.com>
2022-10-27 15:37:55 -0700

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -10,11 +10,11 @@ const fs = require('fs');
 
 // Validate number of arguments
 if (process.argv.length < 4) {
-	console.error(
-		`\x1b[38;2;255;119;119m%s\x1b[0m',
-		`Error: Number of arguments is invalid'
-	);
-	process.exit(-1);
+  console.error(
+    '\x1b[38;2;255;119;119m%s\x1b[0m',
+    'Error: Number of arguments is invalid'
+  );
+  process.exit(-1);
 }
 // Destructure
 const [target, ...sourceFiles] = process.argv.splice(2).reverse();
@@ -22,29 +22,28 @@ const [target, ...sourceFiles] = process.argv.splice(2).reverse();
 /**
  * readToWrite. Write to a destination file from many
  * source files
+ * @param { Array } sourceFiles - Source file list
+ * @param { String } target - Destination file path
  * @return { Number } 1 if successful. -1 if it fails
- * @parameter { Array } sourceFiles - Source file list
- * @parameter { String } target - Destination file path
  */
-
 function readToWrite (filesList = sourceFiles.reverse(), targetFile = target) {
-	try {
-		let data = '';
+  try {
+    let data = '';
 
-		// Reading from multiple sources
-		// Store what is read in the data variable
-		for (const pathFile of filesList) {
-			data += fs.readFileSync(pathFile, 'utf8');
-		}
+    // Reading from multiple sources
+    // Store what is read in the data variable
+    for (const pathFile of filesList) {
+      data += fs.readFileSync(pathFile, 'utf8');
+    }
 
-		// Write to the destination file
-		fs.writeFileSync(targetFile, data);
+    // Write to te destination file
+    fs.writeFileSync(targetFile, data);
 
-		return 1;
-	      } catch (err) {
-		console.error(err);
-	      }
-	}
+    return 1;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 readToWrite();
 
