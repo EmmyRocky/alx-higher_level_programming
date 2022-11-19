62610536ac3e67645f2bfd21cd7cc98d74c6be53
EmmyRocky <emmyrockyy@gmail.com>
2022-11-19 01:30:16 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This is Where we start working in oop way """
+
+
+class Square():
+
+    def __init__(self, size):
+        """ begin the class attribute
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
+
