c373f5d155236c138263f740c224d920430b084d
EmmyRocky <emmyrockyy@gmail.com>
2022-11-19 05:40:31 -0800

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,13 +34,13 @@ class Square():
     def position(self, value):
         """ determine position """
         if (type(value) is not tuple):
-            raise TypeError
-        elif (len(value) != 2):
-            raise TypeError
+            raise TypeError("position has to be a tuple of two positive integers")
+        elif (len(value) is not 2):
+            raise TypeError("position has to be a tuple of 2 positive integers")
         elif (type(value[0]) is not int) or (type(value[1]) is not int):
-            raise TypeError
+            raise TypeError("position has to be a tuple of 2 positive integers")
         elif (value[0] < 0) or (value[1] < 0):
-            raise TypeError
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
 
