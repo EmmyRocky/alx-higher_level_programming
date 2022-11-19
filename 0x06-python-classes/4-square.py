0952c3eaddbcadd303a2dcd991e9291fa238b5ee
EmmyRocky <emmyrockyy@gmail.com>
2022-11-19 02:35:21 -0800

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" We are exploring more about class """
+
+
+class Square:
+    """ square class describing the size & proper validation """
+
+    def __init__(self, size=0):
+        """ Here Initilizes the data """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Here draws in the size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ sets the size to a value """
+        if not isinstance(value, int):
+            raise TypeError("size must be an int")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Sends back the current square area """
+        return self.__size ** 2
